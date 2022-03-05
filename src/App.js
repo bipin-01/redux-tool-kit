9557bbcb730e5eb1ba87aa84568ce44d0973e90a
bipin-01 <poudel7700@gmail.com>
2022-03-05 16:30:32 +0545

    Completed redux-toolkit concept project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { Fragment } from 'react';
 import Header from "./components/Header";
 import Auth from "./components/Auth";
 import Counter from './components/Counter';
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
+
+
 
 
 function App() {
+
+  const isAuth = useSelector(state => state.auth.isAuthenticated)
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth&& <UserProfile />}
        <Counter />
     </Fragment>
    
